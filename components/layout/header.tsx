9cdf2f799ea38f261dee8e5d797a4cc6032f9dfb
Thành Hưng <nguyenthanhung1912@gmail.com>
2025-03-25 00:01:52 +0700

    fixing

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,14 +1,17 @@
+"use client"
+import { usePathname } from "next/navigation";
 import { Logo } from "@/components/layout/logo"
 import { Navbar } from "@/components/layout/navbar"
 import { AuthButtons } from "@/components/layout/auth-buttons"
 import { MobileMenu } from "@/components/layout/mobile-menu"
 
 export function Header() {
+  const pathname = usePathname();
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-16 items-center justify-between">
         <Logo />
-        <Navbar />
+        {pathname !== "/sketches" && <Navbar />}
         <div className="flex items-center">
           <div className="hidden md:block">
             <AuthButtons />
